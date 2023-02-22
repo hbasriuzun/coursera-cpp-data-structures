6ee356c0bb0a2ff53e5111385d36bd298d7588f6
Hasan Basri Uzun <102587414+hbasriuzun@users.noreply.github.com>
2023-02-22 11:29:47 +0300

    up

---
+++ Deneme.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int n;
+    int* arr = nullptr;
+    arr = new int[n];
+
+    if(arr){
+        printf("arr is not null\n");
+    }
+
+    cout << arr << endl;
+
+    return 0;
+}
